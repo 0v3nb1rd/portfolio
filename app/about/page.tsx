85badb58c886494a6604430c2c0331ea23d3d81f
0v3nb1rd <0v3nb1rd@gmail.com>
2025-11-03 16:16:56 +0200

    Add Avatar component and update Home and About pages layout; enhance styling and structure

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,76 +13,105 @@ export default function About() {
     <main className="flex min-h-[calc(100vh-6rem)] flex-col items-center justify-center">
       <section className="py-12">
         <div className="container">
-          <div className="mx-auto grid max-w-3xl grid-cols-2 grid-rows-2 gap-5">
-            <a
-              href="https://github.com/0v3nb1rd"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group hover:bg-muted text-foreground bg-muted/50 relative col-span-full flex h-auto flex-col rounded-md transition-all motion-reduce:transition-none lg:col-span-1 lg:row-span-2 lg:aspect-video"
-            >
-              <div className="flex h-full flex-col items-center justify-center gap-1 p-4">
-                <div className="decoration-muted-foreground flex flex-col items-center gap-2 underline-offset-0 transition-all group-hover:underline group-hover:underline-offset-4">
-                  <GithubSVG className="mb-1" />
-                  <span className="text-3xl font-bold">Github Profile</span>
+          <ul className="mx-auto grid max-w-3xl grid-cols-2 grid-rows-2 gap-5">
+            <li className="col-span-full lg:col-span-1 lg:row-span-2 lg:row-start-1">
+              <a
+                href="https://github.com/0v3nb1rd"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group text-foreground bg-card hover:bg-accent relative flex h-full flex-col rounded-md border shadow transition-all motion-reduce:transition-none"
+              >
+                <div className="flex h-full flex-col items-center justify-center gap-1 p-4">
+                  <div className="decoration-muted-foreground flex flex-col items-center gap-2 underline-offset-0 transition-all group-hover:underline group-hover:underline-offset-4">
+                    <GithubSVG className="mb-1" />
+                    <span className="text-3xl font-bold">Github Profile</span>
+                  </div>
+                  <p className="text-muted-foreground text-center text-xl">Find more of my repositories</p>
                 </div>
-                <p className="text-muted-foreground text-center text-xl">Find more of my repositories</p>
-              </div>
-            </a>
+              </a>
+            </li>
 
-            <a
-              href="https://www.linkedin.com/in/0v3nb1rd/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group hover:bg-muted bg-muted/50 text-foreground relative col-span-full flex h-auto flex-col overflow-hidden rounded-md transition-all motion-reduce:transition-none lg:col-span-1 lg:row-span-1"
-            >
-              <div className="flex h-full items-center justify-center gap-2 p-4">
-                <div className="decoration-muted-foreground flex items-center gap-2 underline-offset-0 transition-all group-hover:underline group-hover:underline-offset-4">
-                  <LinkedInSVG />
-                  <span className="text-center text-xl font-bold">LinkedIn</span>
+            <li className="col-span-full lg:col-span-1 lg:row-span-1 lg:row-start-2">
+              <a
+                href="https://www.linkedin.com/in/0v3nb1rd/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-card text-foreground hover:bg-accent relative flex h-full flex-col overflow-hidden rounded-md border shadow transition-all motion-reduce:transition-none"
+              >
+                <div className="flex h-full items-center justify-center gap-2 p-4">
+                  <div className="decoration-muted-foreground flex items-center gap-2 underline-offset-0 transition-all group-hover:underline group-hover:underline-offset-4">
+                    <LinkedInSVG />
+                    <span className="text-center text-xl font-bold">LinkedIn</span>
+                  </div>
                 </div>
-              </div>
-            </a>
+              </a>
+            </li>
 
-            <a
-              href="mailto:0v3nb1rd@gmail.com"
-              className="group hover:bg-muted bg-muted/50 text-foreground relative col-span-full flex h-auto flex-col overflow-hidden rounded-md transition-all motion-reduce:transition-none lg:col-span-1 lg:row-span-1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="flex h-full items-center justify-center gap-2 p-4">
-                <div className="decoration-muted-foreground flex items-center gap-2 underline-offset-0 transition-all group-hover:underline group-hover:underline-offset-4">
-                  <LetterSVG />
-                  <span className="text-center text-xl font-bold text-balance">0v3nb1rd@gmail.com</span>
+            <li className="col-span-full lg:col-span-1 lg:row-span-1">
+              <a
+                href="mailto:0v3nb1rd@gmail.com"
+                className="group bg-card text-foreground hover:bg-accent relative flex h-full flex-col overflow-hidden rounded-md border shadow transition-all motion-reduce:transition-none"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex h-full items-center justify-center gap-2 p-4">
+                  <div className="decoration-muted-foreground flex items-center gap-2 underline-offset-0 transition-all group-hover:underline group-hover:underline-offset-4">
+                    <LetterSVG />
+                    <span className="text-center text-xl font-bold text-balance">0v3nb1rd@gmail.com</span>
+                  </div>
                 </div>
-              </div>
-            </a>
+              </a>
+            </li>
 
-            <div className="bg-muted/50 col-span-full h-full rounded-md">
-              <div className="flex h-full flex-col items-center justify-center gap-1 p-4">
+            <li className="col-span-full h-full">
+              <div className="bg-card flex h-full flex-col items-center justify-center gap-1 rounded-md border p-4 shadow">
                 <h1 className="text-center text-3xl font-bold">Contact with me</h1>
 
                 <p className="text-muted-foreground text-center text-xl">
                   You can also get in touch with me through this form below.
                 </p>
 
-                <form className="mt-8 mb-4 h-auto w-full max-w-xl">
+                <form className="mt-8 mb-4 h-auto w-full max-w-lg">
                   <LabelInputContainer>
-                    <Input name="name" placeholder="Name" type="text" required />
+                    <Input
+                      className="bg-background border-input border"
+                      name="name"
+                      type="text"
+                      placeholder="Name"
+                      required
+                    />
                   </LabelInputContainer>
                   <LabelInputContainer>
-                    <Input name="email" placeholder="Email" type="email" required />
+                    <Input
+                      className="bg-background border-input border"
+                      name="email"
+                      placeholder="Email"
+                      type="email"
+                      required
+                    />
                   </LabelInputContainer>
                   <LabelInputContainer>
-                    <Textarea name="message" placeholder="Message" className="resize-none" rows={5} required />
+                    <Textarea
+                      className="bg-background border-input resize-none border"
+                      rows={5}
+                      name="message"
+                      placeholder="Message"
+                      required
+                    />
                   </LabelInputContainer>
                   <Input type="hidden" name="website" className="honeypot" />
-                  <Button type="submit" variant="outline" size="lg" className="w-full font-bold">
+                  <Button
+                    type="submit"
+                    variant="outline"
+                    size="lg"
+                    className="bg-background! hover:bg-accent! border-input w-full cursor-pointer font-bold shadow"
+                  >
                     Send
                   </Button>
                 </form>
               </div>
-            </div>
-          </div>
+            </li>
+          </ul>
         </div>
       </section>
     </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,114 @@
+import Image from "next/image";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Home() {
   return (
     <main>
       <section className="flex min-h-[calc(100vh-6rem)] flex-col items-center justify-center">
         <div className="container">
           <div className="py-12">
-            <div className="mx-auto grid max-w-5xl grid-cols-4 grid-rows-4 gap-5 lg:grid-cols-3">
-              <div className="height-auto text-foreground relative col-span-full flex flex-col overflow-hidden rounded-md outline-none motion-reduce:transition-none lg:col-span-1 lg:col-start-2 lg:row-span-2 lg:row-start-2">
-                <div className="bg-muted/50 col-span-full flex h-full items-center justify-center rounded-md">
-                  <h1 className="h1">1 Main</h1>
-                </div>
-              </div>
+            <ul className="--grid-rows-8 mx-auto grid max-w-6xl grid-cols-4 gap-5 lg:grid-cols-3">
+              <li className="height-auto col-span-full lg:col-span-1 lg:col-start-2 lg:row-span-2 lg:row-start-2">
+                <div className="bg-card text-foreground relative col-span-full flex h-full items-center justify-center overflow-hidden rounded-md border shadow">
+                  <div className="flex flex-col items-center justify-center gap-2">
+                    <Avatar className="size-24 text-2xl">
+                      <AvatarImage
+                        alt="photo of Nazar"
+                        src="https://res.cloudinary.com/dcey6ej58/image/upload/w_400,h_400,c_fill,g_auto,q_auto,f_auto/linkedin_1_gvhlsr.png"
+                      />
+                      <AvatarFallback>N</AvatarFallback>
+                    </Avatar>
 
-              <div className="height-auto text-foreground relative col-span-full flex flex-col overflow-hidden outline-none motion-reduce:transition-none lg:col-span-2 lg:row-span-1 lg:row-start-1">
-                <div className="bg-muted/50 col-span-full flex h-full items-center justify-center rounded-md">
-                  <h1 className="h1">2 Section</h1>
+                    <h1 className="h1">Nazar Khaylo</h1>
+                    <p className="text-muted-foreground text-center text-lg">Find more of my repositories</p>
+                  </div>
                 </div>
-              </div>
+              </li>
 
-              <div className="height-auto text-foreground relative col-span-full row-start-2 row-end-3 flex flex-col overflow-hidden outline-none motion-reduce:transition-none lg:col-span-1 lg:col-start-1 lg:row-start-2">
-                <div className="bg-muted/50 col-span-full flex h-full items-center justify-center rounded-md">
-                  <h1 className="h1">3 Section</h1>
+              <li className="height-auto relative col-span-full flex flex-col lg:col-span-2 lg:row-span-1 lg:row-start-1">
+                <div className="bg-card text-foreground relative col-span-full flex h-full items-center justify-center overflow-hidden rounded-md border p-4 shadow">
+                  <div className="flex flex-col gap-2">
+                    <h1 className="h1">Who am I?</h1>
+                    <p className="text-muted-foreground text-lg">
+                      Hello! I’m a full-stack developer passionate about building clear, fast and purposeful software. I
+                      love clean design, structured code and learning from smart people. For me, development isn’t just
+                      a job — it’s how I express creativity and logic together. Currently I focus on building scalable
+                      frontends with React and robust APIs with Node.
+                    </p>
+                  </div>
                 </div>
-              </div>
+              </li>
 
-              <div className="height-auto text-foreground relative col-span-2 hidden flex-col overflow-hidden outline-none motion-reduce:transition-none lg:col-span-1 lg:col-start-3 lg:row-span-1 lg:row-start-3 lg:flex">
-                <div className="bg-muted/50 col-span-full flex h-full items-center justify-center rounded-md">
-                  <h1 className="h1">4 Section</h1>
+              <li className="height-auto relative col-span-full row-start-2 row-end-3 flex flex-col lg:col-span-1 lg:col-start-1 lg:row-start-2">
+                <div className="bg-accent text-foreground relative col-span-full flex h-full items-center justify-center overflow-hidden rounded-md border p-4 shadow">
+                  <h1 className="h1">Frontend Developer</h1>
                 </div>
-              </div>
+              </li>
 
-              <div className="height-auto text-foreground relative col-span-full flex flex-col overflow-hidden outline-none motion-reduce:transition-none lg:col-span-1 lg:col-start-1 lg:row-span-2 lg:row-start-3">
-                <div className="bg-muted/50 col-span-full flex h-full items-center justify-center rounded-md">
-                  <h1 className="h1">5 Section</h1>
+              <li className="height-auto relative col-span-2 hidden flex-col lg:col-span-1 lg:col-start-3 lg:row-span-1 lg:row-start-3 lg:flex">
+                <div className="bg-accent text-foreground relative col-span-full flex h-full items-center justify-center overflow-hidden rounded-md border p-4 shadow">
+                  <div className="flex flex-col gap-2 text-center">
+                    <h1 className="h1">“I really like creating software solutions”</h1>
+                  </div>
                 </div>
-              </div>
+              </li>
 
-              <div className="relative hidden lg:col-start-3 lg:row-span-2 lg:row-start-1 lg:block lg:h-[350px]">
-                <div className="bg-muted/50 col-span-full flex h-full items-center justify-center rounded-md">
-                  <h1 className="h1">6 Section</h1>
+              <li className="height-auto relative col-span-full flex flex-col lg:col-span-1 lg:col-start-1 lg:row-span-2 lg:row-start-3">
+                <div className="bg-card text-foreground relative col-span-full flex h-full items-center justify-center overflow-hidden rounded-md border p-4 shadow">
+                  {/* <h1 className="h1">Stack of technologies</h1> */}
+                  <div className="flex flex-col gap-2">
+                    <h1 className="h1">Tools I use</h1>
+                    <p className="text-muted-foreground text-lg">
+                      I am a student of the National University of Kyiv, where I study at the Faculty of Computer
+                      Science and Cybernetics. I am a student of the National University of Kyiv, where I study at the
+                      Faculty of Computer Science and Cybernetics. I am a student of the National University of Kyiv,
+                      where I study at the Faculty of Computer Science and Cybernetics. I am a student of the National
+                      University of Kyiv, where I study at the Faculty of Computer Science and Cybernetics.
+                    </p>
+                  </div>
                 </div>
-              </div>
+              </li>
 
-              <div className="height-auto text-foreground relative col-span-full flex flex-col overflow-hidden outline-none motion-reduce:transition-none lg:col-span-2 lg:col-start-2 lg:row-span-1 lg:row-start-4">
-                <div className="bg-muted/50 col-span-full flex h-full items-center justify-center rounded-md">
-                  <h1 className="h1">7 Section</h1>
+              <li className="relative hidden lg:col-start-3 lg:row-span-2 lg:row-start-1 lg:block lg:h-[350px]">
+                <div className="bg-card text-foreground relative h-full w-full overflow-hidden rounded-md border shadow">
+                  <Image
+                    fill
+                    className="object-cover"
+                    src="https://res.cloudinary.com/dcey6ej58/image/upload/v1762178905/5-bg_rmuywa.jpg"
+                    alt="relaxing image"
+                  />
                 </div>
-              </div>
-            </div>
+              </li>
+
+              <li className="height-auto relative col-span-full flex flex-col lg:col-span-2 lg:col-start-2 lg:row-span-1 lg:row-start-4">
+                <div className="bg-card text-foreground relative col-span-full flex h-full items-center justify-center overflow-hidden rounded-md border p-4 shadow">
+                  <div className="flex flex-1 flex-col gap-2">
+                    <h1 className="h1">Experience</h1>
+                    <ul className="flex flex-col gap-1">
+                      <li className="flex flex-col">
+                        <b className="text-lg">- Freelance Developer (2023–now)</b>
+                        <i className="text-muted-foreground">
+                          Building modern web apps for small businesses and creative teams.
+                        </i>
+                      </li>
+                      <li className="flex flex-col">
+                        <b className="text-lg">- Full-stack Developer (2022)</b>
+                        <i className="text-muted-foreground">
+                          Faculty of Computer Science and Cybernetics, National University of Kyiv
+                        </i>
+                      </li>
+                      <li className="flex flex-col">
+                        <b className="text-lg">- Full-stack Developer (2020–2022)</b>
+                        <i className="text-muted-foreground">
+                          Building modern web apps for small businesses and creative teams.
+                        </i>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </section>
