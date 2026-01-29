f85841aa8b43faeaa267a23d2cf315ca6af00324
0v3nb1rd <0v3nb1rd@gmail.com>
2026-01-29 19:05:41 +0200

    Refactor HomeGrid animation properties for smoother transitions and update font configuration to use 'swap' display.

--- config/fonts.ts
+++ config/fonts.ts
@@ -3,11 +3,13 @@ import { Inter, Manrope } from "next/font/google";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
+  display: "swap",
 });
 
 const manrope = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
+  display: "swap",
 });
 
 export { inter, manrope };
